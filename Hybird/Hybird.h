d0e995882a2963a57e98060cf21470a14ffbbb13
HanYong <1152829171@qq.com>
2020-07-20 15:13:15 +0800

    install

---
+++ Hybird/Hybird.h
@@ -0,0 +1,39 @@
+//
+//  Hybird.h
+//  Hybird
+//
+//  Created by HanYong on 2020/7/15.
+//  Copyright © 2020 HanYong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hybird.
+FOUNDATION_EXPORT double HybirdVersionNumber;
+
+//! Project version string for Hybird.
+FOUNDATION_EXPORT const unsigned char HybirdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hybird/PublicHeader.h>
+
+#import <Cordova/CDV.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVAvailability.h>
+#import <Cordova/CDVAvailabilityDeprecated.h>
+#import <Cordova/CDVAppDelegate.h>
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVPluginResult.h>
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegate.h>
+#import <Cordova/CDVCommandQueue.h>
+#import <Cordova/CDVConfigParser.h>
+#import <Cordova/CDVInvokedUrlCommand.h>
+#import <Cordova/CDVPlugin+Resources.h>
+#import <Cordova/CDVWebViewEngineProtocol.h>
+#import <Cordova/NSDictionary+CordovaPreferences.h>
+#import <Cordova/NSMutableArray+QueueAdditions.h>
+#import <Cordova/CDVWebViewUIDelegate.h>
+#import <Cordova/CDVWhitelist.h>
+#import <Cordova/CDVScreenOrientationDelegate.h>
+#import <Cordova/CDVTimer.h>
+#import <Cordova/CDVJSON_private.h>
